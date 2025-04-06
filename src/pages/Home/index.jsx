50eedfa22cf0419899210b3f973d2c5d5e9ccc11
AndreyLeite <96005438+AndreyLeite@users.noreply.github.com>
2025-04-06 09:13:08 -0300

    Atualização Home Index

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,32 @@
-import { Container, ChampionshipContainer, ChampionshipButton } from "./styles";
+// filepath: c:\Users\andre\Downloads\Projeto-web-front-main\Projeto-web-front-main\src\pages\Home\index.jsx
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { Container, ChampionshipContainer, ChampionshipButton } from './styles';
 
 export default function ChampionshipMenu() {
+    const [campeonatos, setCampeonatos] = useState([]);
+
+    useEffect(() => {
+        async function fetchCampeonatos() {
+            try {
+                const response = await api.get('/campeonato/campeonatos');
+                setCampeonatos(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar campeonatos:', error);
+            }
+        }
+        fetchCampeonatos();
+    }, []);
+
     return (
         <Container>
             <ChampionshipContainer>
-                <ChampionshipButton>Criar Novo Campeonato</ChampionshipButton>
-                <ChampionshipButton>Editar Seus Campeonatos</ChampionshipButton>
-                <ChampionshipButton>Deletar Seus Campeonatos</ChampionshipButton>
+                <h1>Campeonatos</h1>
+                {campeonatos.map((campeonato) => (
+                    <ChampionshipButton key={campeonato.id}>
+                        {campeonato.nome}
+                    </ChampionshipButton>
+                ))}
             </ChampionshipContainer>
         </Container>
     );
